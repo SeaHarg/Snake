73c227b009f0d1b3ba90610d5da66b282ad5f342
Sean Hargrave <sean.hargrave@hotmail.com>
2012-06-07 09:41:39 -0400

    UserCollides
    User now stops moving when it collides with the wall

--- prjXNA/prjXNA/prkXNA/prjXNA/prjXNAGame/Snake.cs
+++ prjXNA/prjXNA/prkXNA/prjXNA/prjXNAGame/Snake.cs
@@ -17,6 +17,10 @@ namespace prjXNAGame
             Border[1] = new sprite(this, "Side", 800, 10, 0, 0);
             Border[2] = new sprite(this, "Side", 10, 800, 400, 0);
             Border[3] = new sprite(this, "side", 800, 50, 0, 400);
+            Border[0].enableCollision(false);
+            Border[1].enableCollision(false);
+            Border[2].enableCollision(false);
+            Border[3].enableCollision(false);
 
         }
         public void UserIni()
@@ -27,13 +31,31 @@ namespace prjXNAGame
         public void UserLogic()
         {
             if (this.isKeyPressed(Microsoft.Xna.Framework.Input.Keys.Up))
-                BodySprite.setVelocity(0,-10);
+                BodySprite.setVelocity(0,-20);
             else if (this.isKeyPressed(Microsoft.Xna.Framework.Input.Keys.Down))
-                BodySprite.setVelocity(0, 10);
+                BodySprite.setVelocity(0, 20);
             else if (this.isKeyPressed(Microsoft.Xna.Framework.Input.Keys.Left))
-                BodySprite.setVelocity(10, 0);
+                BodySprite.setVelocity(-20, 0);
             else if (this.isKeyPressed(Microsoft.Xna.Framework.Input.Keys.Right))
-                BodySprite.setVelocity(-10, 0);
+                BodySprite.setVelocity(20, 0);
+            if (BodySprite.pixelCollidesWith(Border[0]))
+            {
+                BodySprite.setVelocity(0, 0);
+            }
+            if (BodySprite.pixelCollidesWith(Border[1]))
+            {
+                BodySprite.setVelocity(0, 0);
+            }
+            if (BodySprite.pixelCollidesWith(Border[2]))
+            {
+                BodySprite.setVelocity(0, 0);
+            }
+            if (BodySprite.pixelCollidesWith(Border[3]))
+            {
+                BodySprite.setVelocity(0, 0);
+            }
+                
+
         }
 
         public override void gameIni()
@@ -41,6 +63,7 @@ namespace prjXNAGame
             base.gameIni();
             this.setScreenSize(400, 400);
             this.setScreenColour(Microsoft.Xna.Framework.Graphics.Color.Green);
+            
             EdgesIni();
             UserIni();
         }
@@ -52,7 +75,7 @@ namespace prjXNAGame
         public override void gameRender()
         {
             base.gameRender();
-            EdgesIni();
+        
             for (int c = 0; c < Border.Length; c++)
             {
                 Border[c].draw();
